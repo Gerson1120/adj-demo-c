48a70fb92eb69fee5813aa74872465a9fdcf1086
Gerson1120 <20223tn144@utez.edu.mx>
2025-10-22 19:52:39 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ client/src/modules/test/test.controller.js
@@ -0,0 +1,18 @@
+const TestController = {};
+const Env = import.meta.env;
+
+const API_URL = `http://${Env.VITE_API_HOST}:${Env.VITE_API_PORT}${Env.VITE_API_BASE}`;
+
+TestController.callToAPI = async () => {
+    await fetch(`${API_URL}/test`,{
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json()).then(res => {
+        console.log(res);
+    }).catch(console.log);
+}
+
+export default TestController;
